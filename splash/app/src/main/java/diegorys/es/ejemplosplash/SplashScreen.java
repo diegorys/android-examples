e5b4d36b2e20cacad776fe40d7e7806fe89f269b
Diego de los Reyes <diegorys@gmail.com>
2018-04-02 22:43:06 +0200

    Splash.

---
+++ splash/app/src/main/java/diegorys/es/ejemplosplash/SplashScreen.java
@@ -0,0 +1,38 @@
+package diegorys.es.ejemplosplash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Diego on 19/11/2015.
+ */
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+}
